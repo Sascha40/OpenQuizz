3bddda1bdef4b27e85a143fa02498044e0cb6b68
Sascha Sallès <sascha.salles@ynov.com>
2019-12-03 11:04:32 +0100

    Initial Commit

---
+++ OpenQuizz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OpenQuizz
+//
+//  Created by Sascha Sallès on 03/12/2019.
+//  Copyright © 2019 saschasalles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
